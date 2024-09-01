88be26c8dfbe599230a5505a5b1ce96d0ba56c26
caiobreno2008 <caiobreno155@gmail.com>
2024-09-01 20:14:45 -0300

    caio breno
    corrigir

---
+++ script (2).js
@@ -0,0 +1,15 @@
+let number1 = parseInt(prompt("Digite um número."))
+let number2 = parseInt(prompt("Digite outro número."))
+
+
+let soma = number1 + number2
+let subtracao = number1 - number2
+let multiplicacao = number1 * number2
+let divisao = number1 / number2
+
+
+alert(`Resultados:`);
+alert(`${number1} + ${number2} = ${soma}`)
+alert(`${number1} - ${number2} = ${subtracao}`)
+alert(`${number1} * ${number2} = ${multiplicacao}`)
+alert(`${number1} / ${number2} = ${divisao}`)
---
+++ script (3).js
@@ -0,0 +1,7 @@
+let primeiroNome = prompt("Digite Seu nome.")
+let sobrenome = prompt("Digite seu sobrenome.")
+let curso = prompt("Qual Seu curso?")
+let idade = number - 2024 (prompt("Qual sua idade?"))
+
+alert (`Óla ${nome} ${ sobrenome}`)
+(`Então vocé estuda ${curso} e tem ${idade}  Anos. Legal!`)
